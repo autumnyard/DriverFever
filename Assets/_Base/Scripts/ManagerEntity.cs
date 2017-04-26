f90708bd64b1ea45305c1543ed3d58d9cbd67f7d
autumnyard <autumnyardworks@gmail.com>
2017-04-26 09:41:44 +0200

    Copied project from BaseProject2D.

---
+++ Assets/_Base/Scripts/ManagerEntity.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+
+public class ManagerEntity : MonoBehaviour
+{
+    #region Variables
+    // Players
+    const int maxPlayers = 2;
+    [SerializeField] private GameObject prefabPlayer;
+    private GameObject[] players = new GameObject[maxPlayers];
+    [HideInInspector] public EntityBase[] playersScript = new EntityBase[maxPlayers];
+
+    // TODO: Enemy management in a dynamic list
+    // TODO: Inanimate entities, like objects, in a dynamic list
+    // TODO: Pickup entities, like items, in a dynamic list
+
+    #endregion
+
+
+    #region Monobehaviour
+    void Awake()
+    {
+        Director.Instance.managerEntity = this;
+    }
+    #endregion
+
+
+    #region Entity Management
+    public void SummonPlayers()
+    {
+        SummonPlayer(0, Vector2.zero);
+    }
+
+    public void SummonPlayer(int which, Vector2 position)
+    {
+        // Instantiate
+        players[which] = Instantiate(prefabPlayer, position, Quaternion.identity, transform) as GameObject;
+        if (players[which] == null)
+        {
+            Debug.LogError("Cannot instantiate player " + which);
+            return;
+        }
+
+        // Get player script
+        playersScript[which] = players[which].GetComponent<EntityBase>();
+        if (players[which] == null)
+        {
+            Debug.LogError("Cannot find player script in player " + which);
+            return;
+        }
+
+        // And now set it
+        playersScript[which].Set( which );
+    }
+
+
+    private void RemoveAllPlayers()
+    {
+        for (int i = 0; i < maxPlayers; i++)
+        {
+            RemovePlayer(i);
+        }
+    }
+
+    private void RemovePlayer(int which)
+    {
+        if (players[which] != null)
+        {
+            Destroy(players[which]);
+            players[which] = null;
+        }
+    }
+    #endregion
+
+
+
+    #region General management
+    public void Reset()
+    {
+        RemoveAllPlayers();
+    }
+    #endregion
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DriverFever
-Driver Fever
+
+## Description
+
+Journey through the highway. Speed, dodge, dash and become its master.
+
+
+## Controls
+
+
+## About
+
+- Everything made by me: Undi (Pablo de la Ossa, http://autumnyard.com)
+
+- Using the wonderful DOTween plugin: http://dotween.demigiant.com
